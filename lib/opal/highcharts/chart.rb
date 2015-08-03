4e7a302a97d3a02528715b9f283b1d4655ae7059
balmoral <colgunn@me.com>
2015-08-04 09:21:41 +1000

    initial commit

--- lib/opal/highcharts/chart.rb
+++ lib/opal/highcharts/chart.rb
@@ -30,7 +30,7 @@ module Highcharts
     # @return [Options]
     def options
       o = `#{self.to_n}.options`
-      log "#{self.class.name}##{__method__} : native options = '#{Native(o)}'"
+      log "#{self.class.name}##{__method__} : native options = '#{Native(o).to_h}'"
       o = Options.new(o)
       log "#{self.class.name}##{__method__} : local options = '#{o}'"
       log "#{self.class.name}##{__method__} : local options = '#{o.inspect}'"
