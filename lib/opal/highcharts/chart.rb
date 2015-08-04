604324c97b547a4187150af653c8f0afac6d280d
balmoral <colgunn@me.com>
2015-08-04 18:05:43 +1000

    initial commit

--- lib/opal/highcharts/chart.rb
+++ lib/opal/highcharts/chart.rb
@@ -44,7 +44,7 @@ module Highcharts
 
     def set_title(title, subtitle)
       `console.log(#{"set_title(#{title}, #{subtitle})"})`
-      self.setTitle(title, subtitle)
+      Native(self).setTitle(title, subtitle)
     end
   end
 end
