5c9a78074a5941babc7a38a125e9d234bccce1bb
balmoral <colgunn@me.com>
2015-08-04 10:46:48 +1000

    initial commit

--- lib/opal/highcharts/base.rb
+++ lib/opal/highcharts/base.rb
@@ -3,7 +3,8 @@ module Highcharts
   class UnsupportedFeature < RuntimeError; end
 
   module Base
-    # include Native
+    include Native
+    extend Native
 
     def log(s)
       %x{ console.log( #{ s } ) }
